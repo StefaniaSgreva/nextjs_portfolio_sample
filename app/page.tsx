710d1af4a094da7e28446378bc5889aa94b9aeeb
Stefania Sgreva <stefaniasgreva@gmail.com>
2024-01-05 15:21:46 +0100

    About Me

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Banner from '@/components/Banner';
+import Aboutme from '@/components/Aboutme';
 
 export default function Home() {
   return (
     <main className='w-full bg-gray-950 text-gray-300 px-4'>
-      <div className='max-w-screen-xl mx-auto'><Banner /></div>
+      <div className='max-w-screen-xl mx-auto lg:pt-10'>
+        <Banner />
+        <Aboutme />
+      </div>
     </main>
   )
 }
--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,7 +3,7 @@ import ProfileImg from "../public/img/profileImg.jpeg";
 
 const Banner = () => {
     return (
-        <div className="py-20 flex flex-col md:flex-row items-center gap-10">
+        <div className="wrapper flex flex-col md:flex-row items-center gap-10">
             {/* Image */}
             <div className="w-44 h-44 rounded-full bg-black">
                 < Image className="w-full h-full rounded-full border-[1px] border-indigo-700 p-2 object-cover" src={ProfileImg} alt="Profile Image"/>
@@ -12,7 +12,7 @@ const Banner = () => {
             <div className="w-3/4 flex flex-col items-center md:items-start gap-2">
                 <h1 className="text-5xl font-bold text-white text-center md:text-start">John Doe</h1>
                 <h2 className="text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-blue-300 via-red-300 to-indigo-300 tracking-wide text-center md:text-start">Full Stack Developer</h2>
-                <p className="text-base tracking-wide text-center md:text-start">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio dolorum deserunt, ea soluta minima vero minus nihil odio est unde, dolor corporis, omnis debitis id amet qui natus ipsam recusandae.</p>
+                <p className="text-base tracking-wide text-center md:text-start">Fueled by innovation and a love for efficient code. I blend creativity and technical expertise to craft user-centric digital solutions. From front-end interfaces to back-end systems, I strive for impactful applications merging function with style.</p>
             </div>
         </div>
     );
