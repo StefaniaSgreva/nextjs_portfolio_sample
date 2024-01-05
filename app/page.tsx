c29ecf55108c8e7be92d7dab02544bed589159b8
Stefania Sgreva <stefaniasgreva@gmail.com>
2024-01-05 14:28:37 +0100

    Banner

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Banner from '@/components/Banner';
 
 export default function Home() {
   return (
-    <main className='w-full bg-gray-950 text-gray-300'>
+    <main className='w-full bg-gray-950 text-gray-300 px-4'>
       <div className='max-w-screen-xl mx-auto'><Banner /></div>
     </main>
   )
