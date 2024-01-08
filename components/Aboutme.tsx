d381d200ce9a5e983b4f1eff88d03fbcce44d48b
Stefania Sgreva <stefaniasgreva@gmail.com>
2024-01-08 10:44:12 +0100

    &apos fix

--- components/Aboutme.tsx
+++ components/Aboutme.tsx
@@ -3,13 +3,10 @@ import {SiInformatica} from "react-icons/si";
 
 const Aboutme = () => {
   return (
-    <div className="wrapper">
-        <Title text="About Me" icon={<SiInformatica />}/>
-        <div className="text-lg tracking-wide flex flex-col gap-6">
-          <div>Hey, I'm John Doe, a passionate full-stack developer with a knack for turning ideas into polished, functional code. From crafting responsive front-end interfaces using React and Angular to architecting robust back-end solutions with Node.js and MongoDB, I thrive on building seamless digital experiences.</div>
-          <div>My journey began with HTML and CSS, evolving into a love for solving complex problems through elegant code. Beyond coding, I'm constantly exploring new technologies and design trends to create impactful and user-centric applications.</div>
-          <div>Let's collaborate on your next project and bring innovation to life through technology!</div>
-        </div>
+    <div className="text-lg tracking-wide flex flex-col gap-6">
+        <div>Hey, I&apos;m John Doe, a passionate full-stack developer with a knack for turning ideas into polished, functional code. From crafting responsive front-end interfaces using React and Angular to architecting robust back-end solutions with Node.js and MongoDB, I thrive on building seamless digital experiences.</div>
+        <div>My journey began with HTML and CSS, evolving into a love for solving complex problems through elegant code. Beyond coding, I&apos;m constantly exploring new technologies and design trends to create impactful and user-centric applications.</div>
+        <div>Let&apos;s collaborate on your next project and bring innovation to life through technology!</div>
     </div>
   );
 };
